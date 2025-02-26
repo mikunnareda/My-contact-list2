baa833290feede0997c971458e27246db28a3d92
Kieumy Dang <snackmikun@gmail.com>
2025-02-25 19:12:27 -0500

    21

--- app/src/main/java/com/example/mycontactlist/ContactMapActivity.java
+++ app/src/main/java/com/example/mycontactlist/ContactMapActivity.java
@@ -1,12 +1,21 @@
 package com.example.mycontactlist;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.io.IOException;
+import java.util.List;
 
 public class ContactMapActivity extends AppCompatActivity {
 
@@ -20,5 +29,39 @@ public class ContactMapActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initGetLocationButton();
     }
+    private void initGetLocationButton() {
+        Button locationButton = (Button) findViewById(R.id.buttonGetLocation);
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editAddress = (EditText) findViewById(R.id.editAddress);
+                EditText editCity = (EditText) findViewById(R.id.editCity);
+                EditText editState = (EditText) findViewById(R.id.editState);
+                EditText editZipCode = (EditText) findViewById(R.id.editZipcode);
+
+                //format for a call to geocoding service
+                String address = editAddress.getText().toString() + ", " +
+                        editCity.getText().toString() + ", " +
+                        editState.getText().toString() + ", " +
+                        editZipCode.getText().toString();
+
+                List<Address> addresses = null; //to hold an address object is declared
+
+                Geocoder geo = new Geocoder(ContactMapActivity.this); //declare and assigne new geocoder object
+                try {
+                    addresses = geo.getFromLocationName(address, 1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                TextView txtLatitude = (TextView) findViewById(R.id.textLatitude);
+                TextView txtLongitude = (TextView) findViewById(R.id.textLongtitude);
+
+                txtLatitude.setText(String.valueOf(addresses.get(0).getLatitude()));
+                txtLongitude.setText(String.valueOf(addresses.get(0).getLongitude()));
+            }
+        });
+    }
+
 }
