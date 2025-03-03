8d5522a0b04f92450908b41e6026ebc113381a22
mikun <snackmikun@gmail.com>
2025-03-02 23:37:54 -0500

    27

--- app/src/main/java/com/example/mycontactlist/ContactMapActivity.java
+++ app/src/main/java/com/example/mycontactlist/ContactMapActivity.java
@@ -1,17 +1,20 @@
 package com.example.mycontactlist;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import com.google.android.gms.location.LocationRequest;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,29 +26,47 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
 import java.io.IOException;
 import java.util.List;
 
-public class ContactMapActivity extends AppCompatActivity {
+public class ContactMapActivity extends AppCompatActivity implements OnMapReadyCallback{
     final int PERMISSION_REQUEST_LOCATION = 101;
-    LocationManager locationManager;
-    LocationListener gpsListener;
-    LocationListener networkListener;
-    Location currentBestLocation;
+    GoogleMap gMap;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationRequest locationRequest;
+    LocationCallback locationCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_contact_map);
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.radioGroupSortBy), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });*/
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        createLocationRequest();
+        createLocationCallBack();
+
         initGetLocationButton();
+        initListButton();
+        initSettingsButton();
+        initMapButton();
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
     }
     private void initGetLocationButton() {
         Button locationButton = (Button) findViewById(R.id.buttonGetLocation);
@@ -179,6 +200,23 @@ public class ContactMapActivity extends AppCompatActivity {
             }
         }
     }
+    private void createLocationRequest(){
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+    private void createLocationCallBack(){
+        locationCallback = new LocationCallback() {
+        @Override
+            public void onLocationResult(LocationResult locationResult){
+                if (locationResult == null){
+                    return;
+                }
+                for ()
+        }
+        }
+    }
     private boolean isBetterLocation(Location location) {
         boolean isBetter = false;
         if (currentBestLocation == null) {
@@ -189,6 +227,31 @@ public class ContactMapActivity extends AppCompatActivity {
             isBetter = true; // If it's newer by more than 5 minutes, use it
         }
         return isBetter;
+    }
+    private void initListButton(){
+        ImageButton ibList = findViewById(R.id.imageButtonList);
+        ibList.setOnClickListener(view -> {
+            Intent intent = new Intent (ContactMapActivity.this, ContactListActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+
+        });
+    }
+    private void initMapButton() {
+        ImageButton ibMap = findViewById(R.id.imageButtonMap);
+        ibMap.setOnClickListener(view -> {
+            Intent intent = new Intent(ContactMapActivity.this, ContactMapActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        });
+    }
+    private void initSettingsButton() {
+        ImageButton ibSettings = findViewById(R.id.imageButtonSettings);
+        ibSettings.setOnClickListener(view -> {
+            Intent intent = new Intent(ContactMapActivity.this, ContactSettingsActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        });
     }
 
 }
